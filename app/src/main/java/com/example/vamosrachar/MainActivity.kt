ef747e2848e41f5c73cfaad03910521a834cb56f
Victor <victorio.deg@gmail.com>
2022-10-14 20:53:35 -0300

    Ola

--- app/src/main/java/com/example/vamosrachar/MainActivity.kt
+++ app/src/main/java/com/example/vamosrachar/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.example.vamosrachar
 
+import android.content.Intent
+import android.media.Image
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.text.DecimalFormat
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() , TextToSpeech.OnInitListener {
     var pessoas: Int = 0
     var valor: Float = 0.0f
-    var resString: String = ""
     var res: Float = 0.0f
 
     var TTS: TextToSpeech? = null;
@@ -23,6 +30,10 @@ class MainActivity : AppCompatActivity() , TextToSpeech.OnInitListener {
         } else{
             res = 0.00f
         }
+        val tvResultado : TextView = findViewById(R.id.texto_resultado)
+        val df = DecimalFormat("#.00")
+        tvResultado.text = "R$ " + df.format(res).toString()
+        Log.v("PDM", "Calculando 2")
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,11 +47,8 @@ class MainActivity : AppCompatActivity() , TextToSpeech.OnInitListener {
 
         val etValor : EditText = findViewById(R.id.valor)
         val etPessoas : EditText = findViewById(R.id.pessoas)
-        val tvResultado : TextView = findViewById(R.id.texto_resultado)
-        resString = getString(R.string.texto_resultado,"RS", res)
-        tvResultado.text = resString
-        //val btnShare : FloatingActionButton = findViewById(R.id.fbShare)
-        //val btnSpeech : FloatingActionButton = findViewById(R.id.fbSound)
+        val TTSBtn : FloatingActionButton = findViewById(R.id.soundButton)
+        val SHAREBtn : FloatingActionButton = findViewById(R.id.shareButton)
 
         etValor.addTextChangedListener(object : TextWatcher {
             override fun onTextChanged(str: CharSequence?, ini: Int, bfr: Int, i: Int) {
@@ -53,15 +61,8 @@ class MainActivity : AppCompatActivity() , TextToSpeech.OnInitListener {
                 }
                 calcular()
             }
-
-            override fun afterTextChanged(str: Editable?) {
-                tvResultado.text = getString(R.string.texto_resultado, "R$".toString(), res)
-                resString = tvResultado.text.toString()
-            }
-
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int){
-                //
-            }
+            override fun afterTextChanged(str: Editable?) {}
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int){}
         })
         etPessoas.addTextChangedListener(object : TextWatcher {
             override fun onTextChanged(str: CharSequence, ini: Int, bfr: Int, i: Int){
@@ -72,23 +73,38 @@ class MainActivity : AppCompatActivity() , TextToSpeech.OnInitListener {
                 }
                 calcular();
             }
-            override fun afterTextChanged(str: Editable){
-                tvResultado.text = getString(R.string.texto_resultado,"R$", res);
-                resString = tvResultado.text.toString();
-
-            }
-            override fun beforeTextChanged(str: CharSequence, ini: Int, i: Int, fim: Int){
-                //
-            }
+            override fun afterTextChanged(str: Editable){}
+            override fun beforeTextChanged(str: CharSequence, ini: Int, i: Int, fim: Int){}
         })
-
-
-
-
-
+        TTSBtn.setOnClickListener{
+            val ttsText: String = res.toString() + " for each motherfucking person";
+            speak(ttsText.toString());
+        }
+        SHAREBtn.setOnClickListener{
+            share(res.toString() + " " + getString(R.string.para_cada));
+        }
     }
-
     override fun onInit(p0: Int) {
-        TODO("Not yet implemented")
+        if (p0 == TextToSpeech.SUCCESS) {
+            val result = TTS?.setLanguage(Locale.US)
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED){}
+        }
+    }
+    fun speak(content : String){TTS?.speak(content, TextToSpeech.QUEUE_FLUSH,null,"")}
+
+    fun share(content : String){
+        val intent : Intent = Intent().apply{
+            action = Intent.ACTION_SEND;
+            putExtra(Intent.EXTRA_TEXT, content);
+            type = "text/plain"
+        }
+        val shareIntent = Intent.createChooser(intent,null);
+        startActivity(shareIntent);
+    }
+    override fun onDestroy(){
+        if (TTS == null) {return}
+        TTS?.stop();
+        TTS?.shutdown();
+        super.onDestroy();
     }
 }

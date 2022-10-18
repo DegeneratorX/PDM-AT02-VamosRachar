eaee3a528eb36ab7660c485b43efb2964f871dd5
Victor <victorio.deg@gmail.com>
2022-10-18 10:22:58 -0300

    fix

--- app/src/main/java/com/example/vamosrachar/MainActivity.kt
+++ app/src/main/java/com/example/vamosrachar/MainActivity.kt
@@ -1,16 +1,13 @@
 package com.example.vamosrachar
 
 import android.content.Intent
-import android.media.Image
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.widget.EditText
-import android.widget.ImageButton
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.text.DecimalFormat
